dc61d41f41b9f76ad33d0613236cd19e539d7566
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-07-09 22:34:59 +0430

    first commit

---
+++ src/events/payment.js
@@ -0,0 +1,10 @@
+export default class Payment {
+    constructor(subject) {
+        this.paymentSubject = this.paymentSubject
+    }
+
+    creditCard(paymentData) {
+        console.log(`\na payment occurred ${payment.description}`);
+        this.paymentSubject.notify(paymentData)
+    }
+}
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+import { expect, test, describe } from "@jest/globals";
+
+describe("Test Suite for Observer Pattern", () => {
+    test("# env test", () => {
+        expect(true).toBe(true)
+    })
+})
