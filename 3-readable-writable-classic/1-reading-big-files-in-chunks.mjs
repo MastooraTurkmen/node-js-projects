9f957c1f1734e84d8f64853d35120039aca1663d
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-07-16 22:38:30 +0430

    reading big files

--- 3-readable-writable-classic/1-reading-big-files-in-chunks.mjs
+++ 3-readable-writable-classic/1-reading-big-files-in-chunks.mjs
@@ -1,1 +1,43 @@
 // for i in `seq 1 3`; do node -e "process.stdout.write('hello world'.repeat(1e7))" >> big.file; done
+
+import { createReadStream, promises, statSync } from 'node:fs';
+const fileName = 'big.file';
+
+try {
+    const file = await promises.readFile(fileName)
+    console.log('file buffer', file)
+    console.log('file size', file.byteLength / 1e9, "GB", "\n")
+} catch (error) {
+    console.log('error: max 2GB reached..', error.message)
+}
+
+const { size } = statSync(fileName)
+console.log("file size", size / 1e9, "GB", "\n")
+
+let chunkConsumed = 0
+
+const stream = createReadStream(fileName)
+
+    // 65K per readable! 
+    .once('data', msg => {
+        console.log('on data length', msg.toString().length)
+    })
+
+    .once('readable', _ => {
+        // this stream.read(11) will trigger the on(data) event
+        console.log('read 11 chunk bytes', stream.read(11).toString())
+        console.log('read 05 chunk bytes', stream.read(4).toString())
+
+        chunkConsumed += 11
+    })
+    .on('readable', _ => {
+        let chunk;
+        // stream.read() reads max 65K bytes at once
+        while (null !== (chunk = stream.read())) {
+            chunkConsumed += chunk.length;
+        }
+    })
+
+    .on('end', () => {
+        console.log(`Read ${chunkConsumed} bytes of data..`)
+    })
