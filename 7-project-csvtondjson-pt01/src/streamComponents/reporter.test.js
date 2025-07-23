f06048c8ffdd05fba2b05c4eeb5ea2e02ff9fbac
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-07-23 21:16:04 +0430

    csv project

---
+++ 7-project-csvtondjson-pt01/src/streamComponents/reporter.test.js
@@ -0,0 +1,27 @@
+import { expect, describe, it, jest, afterAll } from "@jest/globals";
+import { log } from "../util.js";
+import readline from "node:readline";
+import Reporter from "./reporter.js";
+
+describe("Reporter Test suite", () => {
+    it('it should print progress status correctly', () => {
+        const loggerMock = jest.fn()
+        const reporter = new Reporter({
+            logger: loggerMock
+        })
+        const multipleCalls = 10
+        const progress = reporter.progress(multipleCalls)
+
+        for (let i = 1; i < multipleCalls; i++) {
+            progress.write('1')
+        }
+        progress.emit('end')
+        expect(loggerMock.mock.calls.length).toEqual(10)
+
+        for (const index in loggerMock.mock.calls) {
+            const [call] = loggerMock.mock.calls[index];
+            const expected = (Number(index) + 1) * 10;
+            expect(call).toStrictEqual(`processed ${expected}.00%`)
+        }
+    })
+})
