38494525ef865771b4e04770ca97cf17ed51174d
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-07-24 21:18:55 +0430

    csv project

--- 7-project-csvtondjson-pt01/src/index.js
+++ 7-project-csvtondjson-pt01/src/index.js
@@ -7,8 +7,31 @@ for i in `seq 1 2`; do node -e "process.stdout.write('$i,erick-$i,$i-text,$i\n'.
 import CSVToNDJSON from "./streamComponents/csvtondjson";
 import { log } from "./util";
 import Reporter from "./streamComponents/reporter.js";
-import { statSync, createReadStream } from "node:fs";
+import { statSync, createReadStream, createWriteStream } from "node:fs";
+import { pipeline, Readable, Transform } from "node:stream";
 
 const { size } = statSync('./big.csv')
 const reporter = new Reporter()
-const fileName = './big.csv'
+const fileName = 'big.csv'
+
+const { size: fileSize } = statSync(fileName)
+const processData = Transform({
+    transform(chunk, encoding, callback) {
+        const data = JSON.parse(chunk)
+        const result = JSON.stringify({ ...data, id: counter++ }).concat('\n')
+        callback(null, result)
+    }
+})
+
+const csvToNDJSON = new CSVToNDJSON({
+    delimiter: ',',
+    headers: ['id', 'name', 'desc', 'age'],
+})
+
+await pipeline(
+    createReadStream(fileName),
+    csvToNDJSON,
+    processData,
+    reporter.progress(fileSize),
+    createWriteStream('big.ndjson')
+)
