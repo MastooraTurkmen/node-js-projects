8e694d50be0a586cdb03d5ab9d9eab34df567e68
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-07-25 14:52:48 +0430

    csv project

--- 7-project-csvtondjson-pt01/src/streamComponents/reporter.js
+++ 7-project-csvtondjson-pt01/src/streamComponents/reporter.js
@@ -5,6 +5,7 @@ const A_HUNDRED_PERCENT = 100;
 
 export default class Reporter {
     #loggerFn
+    LINE_LENGTH_AFTER_TURNED_INTO_JSON = 40
     constructor({ logger = log }) {
         this.#loggerFn = logger;
     }
@@ -12,7 +13,7 @@ export default class Reporter {
     #onData(amount) {
         let totalChunks = 0
         return (chunk) => {
-            totalChunks += chunk.length
+            totalChunks += chunk.length - this.LINE_LENGTH_AFTER_TURNED_INTO_JSON;
             const processed = A_HUNDRED_PERCENT / amount * totalChunks;
             this.#loggerFn(`processed ${processed.toFixed(2)}%`);
         }
