10c980e5d4cf58b1f348109725c373e3d6cd4f55
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-07-27 09:29:48 +0430

    finished async stream

--- module03/02-streams-operators-sql-lite/src/index.js
+++ module03/02-streams-operators-sql-lite/src/index.js
@@ -1,13 +1,17 @@
 // transform - SQL data (sqlite) to noSQL (json data)
 
 import sqlite3 from "sqlite3"
-import { promisify } from "node:util"
 import { Readable } from "node:stream"
+import { pipeline } from "node:stream/promises"
+import { promisify } from "node:util"
+import { createWriteStream } from "node:fs"
+
 const connection = sqlite3.verbose()
 const db = new connection.Database('./data/db')
 const serializeAsync = promisify(db.serialize.bind(db))
 const runAsync = promisify(db.run.bind(db))
-const FindAllAsync = promisify(db.all.bind(db))
+const findAllAsync = promisify(db.all.bind(db))
+console.time('sql-to-ndjson')
 
 
 await serializeAsync()
@@ -17,18 +21,38 @@ async function* selectAsStream() {
     let skip = 0;
 
     while (true) {
-        const data = await FindAllAsync(
+        const data = await findAllAsync(
             `SELECT * FROM users LIMIT ${defaultLimit} OFFSET ${skip}`
         )
         skip += defaultLimit;
 
         // if we've consumed all data it is gonna stop!
         if (!data.length) break;
-        console.log('data', data)
-        for (const item of data) yield item
 
+        for (const item of data) yield item
     }
 }
 
+let processed = 0
 const stream = Readable.from(selectAsStream())
-stream.forEach(item => console.log(item))
+
+stream.filter(({ age }) => age > 25 && age < 50)
+    .map(async (item) => {
+        const name = await Promise.resolve(item.name.toUpperCase())
+        return { ...item, name, at: new Date().toISOString() }
+    })
+    .map((item) => {
+        processed++
+        return JSON.stringify(item).concat("\n")
+    })
+// .forEach(item => console.log(item))
+
+// for await (const item of stream) {
+//     console.log('item', item)
+//     // process item
+// }
+
+await pipeline(stream, createWriteStream('./data/output.ndjson'))
+
+console.log(`\nProcess has finished with ${processed} items..`)
+console.timeEnd('sql-to-ndjson')
