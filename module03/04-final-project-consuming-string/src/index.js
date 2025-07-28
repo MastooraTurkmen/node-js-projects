6fd3f8bbc6a13c0b7bb4e894263e5e0642f579f6
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-07-28 10:40:51 +0430

    final project consuming

--- module03/04-final-project-consuming-string/src/index.js
+++ module03/04-final-project-consuming-string/src/index.js
@@ -1,4 +1,53 @@
+import { pipeline } from "node:stream/promises";
 import { renderUi, log } from "./ui.js";
+import { Readable } from "node:stream"
 
-renderUi()
-log('Application started successfully!');
+const API_URL = 'http://localhost:3000';
+
+async function* startConsumeAPI(signal) {
+    const response = await fetch(API_URL, {
+        signal
+    })
+
+    const reader = response.body.getReader()
+    let done = false
+
+    do {
+        const res = await reader.read()
+        done = res.done
+        if (done) break
+
+        const data = Buffer.from(res.value).toString('utf-8').split('\n')
+
+        for (const item of data) {
+            if (!item) continue
+            yield item
+        }
+    }
+
+    while (!done)
+}
+
+function* print(log) {
+    return async function* (stream) {
+        for await (const chunk of stream) {
+            log(chunk.toString())
+        }
+    }
+}
+
+
+async function initialize(signal) {
+    try {
+        const readable = Readable.from(await startConsumeAPI(signal))
+        await pipeline(
+            readable,
+            print(log),
+            { signal }
+        )
+    } catch (error) {
+        if (error.code === "ABORT_ERR") throw error
+    }
+}
+
+renderUi(initialize)
