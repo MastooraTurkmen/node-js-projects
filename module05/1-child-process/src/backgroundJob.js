4183ab03f8a990b34906afe455b2ca45137d4b60
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-08-02 16:40:36 +0430

    child process file

---
+++ module05/1-child-process/src/backgroundJob.js
@@ -0,0 +1,44 @@
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import split from "split"
+
+console.log('initializing', process.pid)
+
+// once just to get the first message and then die
+process.on("message", async msg => {
+
+    try {
+        await pipeline(
+            createReadStream(msg),
+            split(),
+            async function* (source) {
+                let counter = 0
+                for await (const chunk of source) {
+                    // ignore empty lines
+                    if (!chunk.length) continue;
+
+                    if (++counter <= 20) {
+                        throw new Error(
+                            `found some problem in item\n${chunk.toString()}`
+                        )
+                    }
+
+                    const item = JSON.parse(chunk);
+                    if (!item.email.includes("gmail")) {
+                        continue;
+                    }
+
+                    process.send({
+                        status: 'success',
+                        message: item
+                    })
+                }
+            }
+        )
+    } catch (error) {
+        process.send({
+            status: 'error',
+            message: error.message
+        })
+    }
+})
