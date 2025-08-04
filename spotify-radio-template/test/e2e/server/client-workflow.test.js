d832ec58086088dabeb676956a76b123cd2342b4
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-08-04 21:03:13 +0430

    spotify radio template project

---
+++ spotify-radio-template/test/e2e/server/client-workflow.test.js
@@ -0,0 +1,11 @@
+import { it, describe, before, after } from 'node:test'
+import assert from 'node:assert'
+import { getTestServer } from './helpers.js'
+import fs from 'fs'
+import config from './../../../server/config.js' 
+
+describe('API E2E Test suite', () => {
+    it("should not receive data stream if the process in not playing", async (context) => {
+        
+    })
+})
