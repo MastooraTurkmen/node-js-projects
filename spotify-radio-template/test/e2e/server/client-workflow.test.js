89628b7df8534b3b01f293bdfe98bd82b21b453b
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-08-06 20:44:07 +0430

    test

--- spotify-radio-template/test/e2e/server/client-workflow.test.js
+++ spotify-radio-template/test/e2e/server/client-workflow.test.js
@@ -1,8 +1,10 @@
 import { it, describe, before, beforeEach, afterEach, after } from 'node:test'
 import assert from 'node:assert'
-import { getTestServer } from './helpers.js'
+import { getTestServer, pipeAndReadStreamData } from './helpers.js'
 import fs from 'fs'
 import config from './../../../server/config.js'
+
+const RETENTION_PERIOD = 100
 
 describe('API E2E Test suite', () => {
     let _server;
@@ -16,7 +18,7 @@ describe('API E2E Test suite', () => {
         it("should not receive data stream if the process in not playing", async (context) => {
             const { url } = _server
             const onChunk = context.mock.fn()
-
+            await pipeAndReadStreamData(url, onChunk, RETENTION_PERIOD)
             assert.strictEqual(
                 onChunk.mock.callCount(), 0,
                 `Expect onChunk to not have been called, but got ${onChunk.mock.callCount()}`
