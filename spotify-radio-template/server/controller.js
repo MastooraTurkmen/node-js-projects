1933804fdeda19a9eae244d9aebdc9087c75c63b
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-08-07 21:52:01 +0430

    controller

--- spotify-radio-template/server/controller.js
+++ spotify-radio-template/server/controller.js
@@ -2,6 +2,7 @@ import { logger } from './util.js'
 
 class Controller {
   #service
+  #isPlaying = false
 
   constructor({ service }) {
     this.#service = service
@@ -19,6 +20,25 @@ class Controller {
     }
 
     return { stream: clientStream, onClose }
+  }
+
+  handleCommand({ command }) {
+    const result = {
+      result: 'ok',
+    }
+    const cmd = command.toLowerCase()
+    logger.info(`cmd received: ${cmd}`)
+    if (cmd.includes('start') && this.#isPlaying) {
+      this.#isPlaying = true
+
+      return result
+    }
+
+    if (cmd.includes('stop')) {
+      this.#isPlaying = false
+
+      return result
+    }
   }
 
 }
