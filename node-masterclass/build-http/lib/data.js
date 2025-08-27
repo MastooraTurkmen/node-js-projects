cb4fe26db314a77f66bc26567913357c2a1b36b5
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-08-27 20:42:16 +0430

    post, get, put, delete

--- node-masterclass/build-http/lib/data.js
+++ node-masterclass/build-http/lib/data.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs')
 const path = require('path')
-
+const helpers = require('./helpers')
 
 // container for the module to be exported
 
@@ -40,7 +40,12 @@ lib.create = function (dir, file, data, callback) {
 
 lib.read = function (dir, file, callback) {
     fs.readFile(lib.baseDir + dir + '/' + file + '.json', 'utf8', (err, data) => {
-        callback(err, data)
+        if (!err && data) {
+            const parsedData = helpers.parseJsonToObject(data)
+            callback(false, parsedData)
+        } else {
+            callback(err, data)
+        }
     })
 }
 
