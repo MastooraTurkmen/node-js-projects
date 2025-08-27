0ba7b53f37c8586c4102952cf1693fafb6e0970b
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-08-27 20:42:19 +0430

    post, get, put, delete

---
+++ node-masterclass/build-http/lib/helpers.js
@@ -0,0 +1,27 @@
+// container for all the handlers
+
+const crypto = require('crypto')
+const config = require('./config')
+const helpers = {}
+
+// create a SHA256 hash
+helpers.hash = (str) => {
+    if (typeof (str) == 'string' && str.length > 0) {
+        const hash = crypto.createHmac('sha256', config.hashingSecret).update(str).digest('hex')
+        return hash
+    } else {
+        return false
+    }
+}
+
+// parse a JSON string to an object in all cases, without throwing
+helpers.parseJsonToObject = (str) => {
+    try {
+        const obj = JSON.parse(str)
+        return obj
+    } catch (error) {
+        return {}
+    }
+}
+
+module.exports = helpers
