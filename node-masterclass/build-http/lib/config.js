8079b52a9afbb9ee19ec7852da4cf232c24d1834
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-08-30 21:20:40 +0430

    check server

--- node-masterclass/build-http/lib/config.js
+++ node-masterclass/build-http/lib/config.js
@@ -4,14 +4,16 @@ environment.staging = {
     'httpPort': 3000,
     'httpsPort': 3001,
     'envName': 'staging',
-    'hashingSecret': 'thisIsASecret'
+    'hashingSecret': 'thisIsASecret',
+    'maxChecks': 5
 }
 
 environment.production = {
     'httpPort': 5000,
     'httpsPort': 5001,
     'envName': 'production',
-    'hashingSecret': 'thisIsASecret'
+    'hashingSecret': 'thisIsASecret',
+    'maxChecks': 5
 }
 
 // determine which environment was passed as a command-line argument
