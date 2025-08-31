99a58294f35f90b39fe986f095de1a83129f9842
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-08-31 20:56:34 +0430

    Add Twilio SMS sending helper function

--- node-masterclass/build-http/lib/helpers.js
+++ node-masterclass/build-http/lib/helpers.js
@@ -1,7 +1,11 @@
 // container for all the handlers
 
 const crypto = require('crypto')
+const https = require('https')
 const config = require('./config')
+const queryString = require('querystring')
+
+// helpers object
 const helpers = {}
 
 // create a SHA256 hash
@@ -44,6 +48,64 @@ helpers.createRandomString = function (strLength) {
         return str;
     } else {
         return false;
+    }
+}
+
+// send an SMS message via Twilio
+helpers.sendTwilioSms = (phone, msg, callback) => {
+    // validate parameters
+    phone = typeof (phone) == 'string' && phone.trim().length == 10 ? phone.trim() : false
+    msg = typeof (msg) == 'string' && msg.trim().length > 0 && msg.trim().length <= 1600 ? msg.trim() : false
+
+    if (phone && msg) {
+        // configure the request payload
+        const payload = {
+            From: config.twilio.fromPhone,
+            To: '+93' + phone,
+            Body: msg
+        }
+
+        // stringify the payload
+        const stringPayload = queryString.stringify(payload)
+
+        // configure the request details
+        const requestDetails = {
+            'protocol': 'https:',
+            'hostname': 'api.twilio.com',
+            'method': 'POST',
+            'path': '/2010-04-01/Accounts/' + config.twilio.accountSid + '/Messages.json',
+            'auth': config.twilio.accountSid + ':' + config.twilio.authToken,
+            'headers': {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(stringPayload)
+            }
+        }
+
+        // instantiate the request object
+        const req = https.request(requestDetails, (res) => {
+            // grab the status of the sent request
+            const status = res.statusCode
+            // callback successfully if the request went through
+            if (status == 200 || status == 201) {
+                callback(false)
+            } else {
+                callback('Status code returned was ' + status)
+            }
+        });
+
+        // bind to the error event so it doesn't get thrown
+        req.on('error', (e) => {
+            callback(e)
+        })
+
+        // add the payload
+        req.write(stringPayload)
+
+        // end the request
+        req.end()
+
+    } else {
+        callback('Given parameters were missing or invalid')
     }
 }
 
