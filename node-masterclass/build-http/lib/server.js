557ad343979b1f0171d426efd274d04f91e77548
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-08-31 20:57:13 +0430

    Implement HTTP and HTTPS server functionality

---
+++ node-masterclass/build-http/lib/server.js
@@ -0,0 +1,113 @@
+const http = require('http');
+const https = require('https');
+const { StringDecoder } = require('string_decoder');
+const url = require('url')
+const config = require('./lib/config')
+const fs = require('fs')
+const handlers = require('./lib/handlers');
+const helpers = require('./lib/helpers');
+const path = require('path');
+
+// Instantiate of http server module object
+let server = {}
+
+server.httpServer = http.createServer((req, res) => {
+    server.unifiedServer(req, res)
+});
+
+// Instantiate of https server
+server.httpsServerOptions = {
+    'key': fs.readFileSync(path.join(__dirname, '/../https/key.pem')),
+    'cert': fs.readFileSync(path.join(__dirname, '/../https/cert.pem'))
+}
+server.httpsServer = https.createServer(server.httpsServerOptions, (req, res) => {
+    server.unifiedServer(req, res)
+})
+
+// start the https server
+
+server.httpsServer.listen(config.httpsPort, () => {
+    console.log(`server is listening on ${config.httpsPort}`)
+})
+
+
+// All the server logic for a http server
+server.unifiedServer = (req, res) => {
+    // get the url and parse it
+    const parsedURL = url.parse(req.url, true)
+
+    // get the path
+    const path = parsedURL.pathname
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+    // get the query string as an object
+    const queryStringObject = parsedURL.query
+
+    // get http method
+    const method = req.method.toLowerCase()
+
+    // get the headers as an object
+    const headers = req.headers
+
+    // get the payload, if any
+    const decoder = new StringDecoder('utf-8')
+    let buffer = ''
+    req.on('data', (data) => {
+        buffer += decoder.write(data)
+    })
+
+    req.on('end', () => {
+        buffer += decoder.end()
+
+        const chosenHandler = typeof (server.router[trimmedPath]) !== 'undefined' ? server.router[trimmedPath] : handlers.notFound
+
+        // construct the data object to send to the handler
+        const data = {
+            'trimmedPath': trimmedPath,
+            "queryStringObject": queryStringObject,
+            "method": method,
+            "headers": headers,
+            "payload": helpers.parseJsonToObject(buffer)
+        }
+
+        chosenHandler(data, (statusCode, payload) => {
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+            payload = typeof (payload) == 'object' ? payload : {};
+
+            const payloadString = JSON.stringify(payload)
+
+            // return the response
+            res.setHeader('Content-Type', 'application/json')
+            res.writeHead(statusCode)
+            res.end(payloadString)
+            console.log('Returning this response:', statusCode, payloadString)
+        })
+    })
+}
+
+// define a request router
+
+server.router = {
+    'ping': handlers.ping,
+    'users': handlers.users,
+    'tokens': handlers.tokens,
+    'checks': handlers.checks
+}
+
+
+// init script
+server.init = () => {
+    // start the http server
+    server.httpServer.listen(config.httpPort, () => {
+        console.log(`server is listening on ${config.httpPort}`)
+    })
+
+    // start the https server
+    httpsServer.listen(config.httpsPort, () => {
+        console.log(`server is listening on ${config.httpsPort}`)
+    })
+}
+
+
+// export the module
+module.exports = server
