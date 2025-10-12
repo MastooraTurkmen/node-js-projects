f75bb454a4014eec5b799b074053d3f1671b4f95
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-10-12 20:09:03 +0430

    http2 server

---
+++ node-masterclass/build-http/misc/http2-server.js
@@ -0,0 +1,23 @@
+// Example HTTP/2 Server
+
+// Dependencies
+const http2 = require('http2');
+
+// Init server
+
+const server = http2.createServer()
+
+
+// On a stream, send back hello world html
+
+server.on('stream', (stream, headers) => {
+    stream.respond({
+        'status': 200,
+        'content-type': 'text/html'
+    })
+    stream.end('<html><body><h1>Hello World</h1></body></html>')
+})
+
+// Listen on port 6000
+
+server.listen(6000)
