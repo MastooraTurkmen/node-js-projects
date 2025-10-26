4576cecce7cc7f246ef56bc2511eea94e5610c36
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-10-26 20:35:41 +0430

    delete product

--- node-masterclass/vanila-node/server.js
+++ node-masterclass/vanila-node/server.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const { getProducts, getProduct, createProduct, updateProduct } = require('./controller/productController')
+const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('./controller/productController')
 const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
@@ -10,11 +10,14 @@ const server = http.createServer((req, res) => {
         getProduct(req, res, id)
     } else if (req.url === '/api/products' && req.method === 'POST') {
         createProduct(req, res)
-    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUTs') {
+    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
         const id = req.url.split('/')[3]
         updateProduct(req, res, id)
     }
-
+    else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE') {
+        const id = req.url.split('/')[3]
+        deleteProduct(req, res, id)
+    }
     else {
         res.writeHead(404, { "Content-Type": "application/json" })
         res.end(JSON.stringify({ message: "Route not found" }))
